9ab44f639645e7dd745d3ca16ca962adac8b8d08
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-02 21:25:12 +0600

    even-odd calculator

---
+++ even-odd-calculator.js
@@ -0,0 +1,10 @@
+// Write a program that checks if a given number is even or odd.
+
+let number = 100;
+
+if (number % 2 === 0){
+    console.log('This is even number')
+}
+else{
+    console.log("this is odd number")
+}
