fdd7121f1e86a38cc2e2734742a5472b81e9bfe6
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-02 21:15:38 +0600

    pass and fail

---
+++ pass-fail.js
@@ -0,0 +1,14 @@
+// <!-- Create a program where:
+
+// If a student scores 40 or more, they pass.
+// If they score less than 40, they fail. -->
+
+
+let studentScore = 0;
+
+if(studentScore >=40){
+    console.log("You are passed")
+}
+else{
+    console.log("You are failed")
+}
